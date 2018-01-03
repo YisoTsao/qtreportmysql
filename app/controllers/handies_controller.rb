fc014a1e41b8cbec341840b81656f02fc4a771ac
gary <yiso05255@gmail.com>
2018-01-03 23:17:18 +0800

    modify pdf file

--- app/controllers/handies_controller.rb
+++ app/controllers/handies_controller.rb
@@ -17,7 +17,7 @@ class HandiesController < ApplicationController
             format.json 
             format.csv { send_data @handies.to_csv }
             format.xls { send_data @handies.to_csv }
-            format.pdf  { render template: 'handies/standreport', pdf: 'Report', layout: 'pdf.html', location: @handy }  # Report is downloade file name
+            format.pdf  { render template: 'handies/report', pdf: 'Report', layout: 'pdf.html', location: @handy }  # Report is downloade file name
             end  
        end 
   end
