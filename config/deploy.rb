05bf5a6fe1a097cf0a2d60c06c1e364b79a24918
gary <yiso05255@gmail.com>
2018-01-15 18:33:36 +0800

    deploy to 10.198.82.210

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,11 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-#Deploy to Lindoe 
- set :deploy_to, "/home/gary/qtreportmysql"
+#Deploy to Lindoe  
+ #set :deploy_to, "/home/gary/qtreportmysql"
 
-#Deploy to Lindoe 
-#set :deploy_to,"/home/deploy/quallist"
+#Deploy to QT server 
+set :deploy_to,"/home/deploy/quallist"
 #set :passenger_restart_with_touch, true
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
