879bc11ea730069b27b936019260a96961e04130
gary <yiso05255@gmail.com>
2018-04-25 17:50:43 +0800

    show.html.erb finished

--- db/migrate/20180420080302_create_istoragessds.rb
+++ db/migrate/20180420080302_create_istoragessds.rb
@@ -2,25 +2,28 @@ class CreateIstoragessds < ActiveRecord::Migration[5.1]
   def change
     create_table :istoragessds do |t|
       t.integer :pccitem
-      t.date :receivedate
+      t.string :receivedate
+      t.string :prodpn
       t.string :solution
       t.string :ftype
-      t.string :capacity
-      t.string :qual
-      t.integer :qty
-      t.date :startdate
-      t.date :finishdate
-      t.string :testres
-      t.string :owner
-      t.text :remark
-      t.string :prodpn
+      t.text :desc
       t.text :fwvversion
+      t.string :capacity
+      t.integer :qty
+      t.string :qual
+      t.string :plandate
+      t.string :startdate
+      t.string :finishdate
+      t.string :testres
+      t.string :status
+      t.string :owner
+      t.text :remark  
       t.string :prodname
       t.string :pcbvers
       t.string :rpname
-      t.date :rpdate
+      t.string :rpdate
       t.text :approvename
-      t.text :desc
+      
 
       t.timestamps
     end
